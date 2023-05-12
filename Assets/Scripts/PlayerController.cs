eeed456ee0ba759924ba00bad548f8b7446014ce
ben-ren <bluebirdyfly@gmail.com>
2023-05-12 17:50:34 +1000

    Tangent's of old endpoint now reallign based on original direction and new endpoint.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,7 +28,30 @@ public class PlayerController : TrainController
     }
 
     /**
+     * Set the new node's tangent's to properly allign rails
+     */
+    void TangentAllign(int index)
+    {
+        //SSC.spline.SetTangentMode(index - 1, ShapeTangentMode.Continuous);
+        Vector3 A = SSC.spline.GetPosition(index);      //end point
+        Vector3 B = SSC.spline.GetPosition(index - 1);  //start point
+        Vector3 C = SSC.spline.GetPosition(index - 2);  //previous penultimate point
+        
+        Vector3 direction = (B-C).normalized;     //the direction the node at that location is trending towards
+
+        //CHECK - distance may need to be divided by 2 for edge cases.
+        float distance = Vector3.Distance(B, A);    //the distance between the original end point and the new end point
+
+        //Sets the tangents of the old end node.
+        SSC.spline.SetLeftTangent(index - 1, -direction);
+        SSC.spline.SetRightTangent(index - 1, direction * distance);
+        
+    }
+
+    /**
      * The player's inputs used to control the actions of the train's PathFollow script.
+     * 
+     * TODO - Replace mousePos with a public Transform that takes in a gameObject alligned with the RailGun.
      */
     void Controls()
     {
@@ -37,6 +60,7 @@ public class PlayerController : TrainController
             Vector3 newPos = new(mousePos.x, mousePos.y, 0.0f);
             AddNode(newPos);
             SetLastPoint();
+            TangentAllign(_spline.GetPointCount()-1);
         }
     }
 }
